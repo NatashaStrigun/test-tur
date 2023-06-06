fb2bd4c7bc5d17efe0c5a4b2f515b1fc17655d3b
Suxrob Shukurov <yurinowa.m@yandex.ru>
2023-06-06 11:16:01 +0300

    added

---
+++ data.js
@@ -0,0 +1,214 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20230412_135255_00_007_pureshot",
+      "name": "IMG_20230412_135255_00_007_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.5534700455491581,
+        "pitch": 0.19635943228125008,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0212912306280408,
+          "pitch": 0.0152258815694708,
+          "rotation": 0,
+          "target": "1-img_20230412_135401_00_008"
+        },
+        {
+          "yaw": 1.4580060714837222,
+          "pitch": 0.10967049755072011,
+          "rotation": 0,
+          "target": "4-img_20230412_175807_00_014"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20230412_135401_00_008",
+      "name": "IMG_20230412_135401_00_008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.7866567382974914,
+        "pitch": 0.07652086345999365,
+        "fov": 0.8124355354142738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4869737992835503,
+          "pitch": 0.0597233768489005,
+          "rotation": 0,
+          "target": "2-smotrjvji-kabinet"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.87001700095675,
+          "pitch": -0.2404347794910855,
+          "title": "Ресепшен",
+          "text": "<div><br></div><div>&nbsp;https://perm.monaliza-clinic.ru/services/</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-smotrjvji-kabinet",
+      "name": "smotrjvji kabinet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03277235886608665,
+          "pitch": 0.17831666164157767,
+          "rotation": 0,
+          "target": "3-operation"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-operation",
+      "name": "operation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4625805295842387,
+          "pitch": 0.13883982609081258,
+          "rotation": 0,
+          "target": "0-img_20230412_135255_00_007_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_20230412_175807_00_014",
+      "name": "IMG_20230412_175807_00_014",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.03229019579998926,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.27574104158361123,
+          "pitch": 0.03121984596594629,
+          "rotation": 0,
+          "target": "0-img_20230412_135255_00_007_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
